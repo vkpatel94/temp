cbf860a27a6c624f669000925e74d69539a263f9
Rushabh <rushabhshah341@gmail.com>
2017-10-06 13:28:56 -0400

    Added all Folders

---
+++ config/assets/default.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/* eslint comma-dangle:[0, "only-multiline"] */
+
+module.exports = {
+  client: {
+    lib: {
+      css: [
+        // bower:css
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/font-awesome/css/font-awesome.min.css',
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
+        // endbower
+      ],
+      js: [
+        // bower:js
+        'public/lib/angular/angular.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'modules/core/client/app/jquery.2.0.2.min.js',
+        'public/lib/ng-file-upload/ng-file-upload.js',
+        'modules/users/client/ng-file-upload/angular-file-upload.min.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-mocks/angular-mocks.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        // endbower
+      ],
+      tests: ['public/lib/angular-mocks/angular-mocks.js']
+    },
+    css: [
+      'modules/*/client/{css,less,scss}/*.css'
+    ],
+    less: [
+      'modules/*/client/less/*.less'
+    ],
+    sass: [
+      'modules/*/client/scss/*.scss'
+    ],
+    js: [
+      'modules/core/client/app/config.js',
+      'modules/core/client/app/init.js',
+      'modules/*/client/*.js',
+      'modules/*/client/**/*.js'
+    ],
+    img: [
+      'modules/**/*/img/**/*.jpg',
+      'modules/**/*/img/**/*.png',
+      'modules/**/*/img/**/*.gif',
+      'modules/**/*/img/**/*.svg',
+      'data/*.jpg',
+      'data/*.png'
+    ],
+    views: ['modules/*/client/views/**/*.html'],
+    templates: ['build/templates.js']
+  },
+  server: {
+    gulpConfig: ['gulpfile.js'],
+    allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
+    models: 'modules/*/server/models/**/*.js',
+    routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
+    sockets: 'modules/*/server/sockets/**/*.js',
+    config: ['modules/*/server/config/*.js'],
+    policies: 'modules/*/server/policies/*.js',
+    views: ['modules/*/server/views/*.html']
+  }
+};
---
+++ config/env/development.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var defaultEnvConfig = require('./default');
+
+module.exports = {
+  db: {
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/PropertyPortal',
+    options: {
+      user: '',
+      pass: ''
+    },
+    // Enable mongoose debug mode
+    debug: process.env.MONGODB_DEBUG || false
+  },
+  log: {
+    // logging with Morgan - https://github.com/expressjs/morgan
+    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+    format: 'dev',
+    fileLogger: {
+      directoryPath: process.cwd(),
+      fileName: 'app.log',
+      maxsize: 10485760,
+      maxFiles: 2,
+      json: false
+    }
+  },
+  app: {
+    title: defaultEnvConfig.app.title + ' - Development Environment'
+  },
+  facebook: {
+    clientID: process.env.FACEBOOK_ID || '709418999253305',
+    clientSecret: process.env.FACEBOOK_SECRET || '938e819cde9411fc6652e58a911fbc7c',
+    callbackURL: '/api/auth/facebook/callback'
+  },
+  twitter: {
+    username: '@TWITTER_USERNAME',
+    clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+    clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+    callbackURL: '/api/auth/twitter/callback'
+  },
+  google: {
+    clientID: process.env.GOOGLE_ID || '748914562188-s840u5fke5teqp2pk6je6alt8co06i8p.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_SECRET || '2dtfM92gz_UR8Xnt4slklhLE',
+    callbackURL: '/api/auth/google/callback'
+  },
+  linkedin: {
+    clientID: process.env.LINKEDIN_ID || 'APP_ID',
+    clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+    callbackURL: '/api/auth/linkedin/callback'
+  },
+  github: {
+    clientID: process.env.GITHUB_ID || 'APP_ID',
+    clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+    callbackURL: '/api/auth/github/callback'
+  },
+  paypal: {
+    clientID: process.env.PAYPAL_ID || 'CLIENT_ID',
+    clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
+    callbackURL: '/api/auth/paypal/callback',
+    sandbox: true
+  },
+  mailer: {
+    from: process.env.MAILER_FROM || 'My Housing',
+    options: {
+      service: process.env.MAILER_SERVICE_PROVIDER || 'gmail',
+      auth: {
+        user: process.env.MAILER_EMAIL_ID || 'tempualbanyse@gmail.com',
+        pass: process.env.MAILER_PASSWORD || 'tempualbanyse8'
+      }
+    }
+  },
+  livereload: false,
+  seedDB: {
+    seed: process.env.MONGO_SEED === 'true',
+    options: {
+      logResults: process.env.MONGO_SEED_LOG_RESULTS !== 'false'
+    },
+    collections: [{
+      model: 'User',
+      docs: [{
+        data: {
+          username: 'local-admin',
+          email: 'admin@localhost.com',
+          firstName: 'Admin',
+          lastName: 'Local',
+          roles: ['admin', 'user']
+        }
+      }, {
+        overwrite: true,
+        data: {
+          username: 'local-user',
+          email: 'user@localhost.com',
+          firstName: 'User',
+          lastName: 'Local',
+          roles: ['user']
+        }
+      }]
+    }, {
+      model: 'Article',
+      options: {
+        // Override log results setting at the
+        // collection level.
+        logResults: true
+      },
+      skip: {
+        // Skip collection when this query returns results.
+        // e.g. {}: Only seeds collection when it is empty.
+        when: {} // Mongoose qualified query
+      },
+      docs: [{
+        data: {
+          title: 'First Article',
+          content: 'This is a seeded Article for the development environment'
+        }
+      }]
+    }]
+  }
+};
