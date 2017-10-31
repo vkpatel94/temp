7fc69056b325018a71ee97a959b991c89872ea3d
Rushabh <rushabhshah341@gmail.com>
2017-10-31 13:55:11 -0400

    commenting out rentals

--- modules/listyourrentals/client/config/listyourrentals.client.config.js
+++ modules/listyourrentals/client/config/listyourrentals.client.config.js
@@ -9,24 +9,24 @@
 
   function menuConfig(menuService) {
     // Set top bar menu items
-    menuService.addMenuItem('topbar', {
-      title: 'Listyourrentals',
-      state: 'listyourrentals',
-      type: 'dropdown',
-      roles: ['*']
-    });
+  //   menuService.addMenuItem('topbar', {
+  //     title: 'Listyourrentals',
+  //     state: 'listyourrentals',
+  //     type: 'dropdown',
+  //     roles: ['*']
+  //   });
 
-    // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'listyourrentals', {
-      title: 'List Listyourrentals',
-      state: 'listyourrentals.list'
-    });
+  //   // Add the dropdown list item
+  //   menuService.addSubMenuItem('topbar', 'listyourrentals', {
+  //     title: 'List Listyourrentals',
+  //     state: 'listyourrentals.list'
+  //   });
 
-    // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'listyourrentals', {
-      title: 'Create Listyourrental',
-      state: 'listyourrentals.create',
-      roles: ['user']
-    });
-  }
+  //   // Add the dropdown create item
+  //   menuService.addSubMenuItem('topbar', 'listyourrentals', {
+  //     title: 'Create Listyourrental',
+  //     state: 'listyourrentals.create',
+  //     roles: ['user']
+  //   });
+  // }
 }());
